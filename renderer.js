41d9e7f15a82865f265ed1b879077fb874cf7ca5
thisisaaroland <aaron@mapzen.com>
2017-04-18 11:30:17 -0700

    try to use keytar and get attacked by node version nonsense

--- renderer.js
+++ renderer.js
@@ -1,13 +1,27 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
 const partyparrot = require("./mapzen.whosonfirst.partyparrot.js");
 
 const config = require("./mapzen.whosonfirst.config.js");
 const api = require("./mapzen.whosonfirst.api.js");
 const spec = require("./mapzen.whosonfirst.api.spec.js");
 const explorer = require("./mapzen.whosonfirst.api.explorer.js");
+
+/*
+
+https://medium.com/@ccnokes/how-to-securely-store-sensitive-information-in-electron-with-node-keytar-51af99f1cfc4
+https://github.com/atom/node-keytar
+
+Sigh... (20170418/thisisaaronland)
+
+Uncaught Error: The module
+'electron-whosonfirst-api-explorer/node_modules/keytar/build/Release/keytar.node'
+was compiled against a different Node.js version using
+NODE_MODULE_VERSION 51. This version of Node.js requires
+NODE_MODULE_VERSION 53. Please try re-compiling or re-installing
+the module (for instance, using `npm rebuild` or`npm install`).
+
+const keytar = require('keytar')	
+api_key = keytar.getPassword("whosonfirst", "api_explorer")
+*/
 
 api.set_handler('authentication', function(){
 	return config.api_key()
