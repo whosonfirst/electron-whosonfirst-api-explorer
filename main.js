1d39ba7f13a3ad02adad040ed0ddfba20c64ed18
thisisaaroland <aaron@mapzen.com>
2017-05-03 22:45:05 -0700

    add menu stuff to ensure copy/paste works (in native apps)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-const Tray = electron.Tray
+const Menu = electron.Menu
 
 const ipcMain = require('electron').ipcMain;
 
@@ -49,7 +49,35 @@ function createWindow () {
 	})
 }
 
-app.on('ready', createWindow)
+app.on('ready', function(){
+
+	createWindow();
+
+	// https://pracucci.com/atom-electron-enable-copy-and-paste.html
+
+	var template = [{
+		label: "Application",
+		submenu: [
+			{ label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+			{ type: "separator" },
+			{ label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+		]}, {
+			label: "Edit",
+			submenu: [
+				{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+				{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+				{ type: "separator" },
+				{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+				{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+				{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+				{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+			]}
+		       ];
+	
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+	
+});
+
 
 app.on('window-all-closed', function () {
 	if (process.platform !== 'darwin') {
